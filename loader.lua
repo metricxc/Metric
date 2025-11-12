8a883d7d81ea5378e1caf84d6689a624eab55eb6
metricxc <iq21fr@gmail.com>
2025-11-12 17:48:56 +0545

    Update and rename main.lua to loader.lua

---
+++ loader.lua
@@ -0,0 +1,2 @@
+--// dsc.gg/metric \\--
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f9d5ea095be48e60ab9f38c7cc8fc8178280aef8d28e5bda4b999934329dd9ce/download"))()
